478653540207ac7c5439de133accf339b90078ae
Kaio Silveira <silveira.kaio@outlook.com>
2022-08-08 18:58:04 +0100

    add in-file mock for timetable

--- src/ticketing-app/carriers/renfe/search/fetch-timetable/index.js
+++ src/ticketing-app/carriers/renfe/search/fetch-timetable/index.js
@@ -1,19 +1,16 @@
+import fs from 'fs';
+
+const createRandomDelay = () => parseInt(Math.random() * 999, 10);
+
 const fetchTimetable = async env => {
   return new Promise(resolve => {
-    const timetable = [];
-
-    for (let i = 0; i < 7; i++) {
-      for (let j = 0; j <= 23; j++) {
-        timetable.push({
-          date: new Date(2022, 8, i, j, 30, 0),
-          origin: 1200,
-          destination: 1300,
-          price: 100000 + parseInt(Math.random() * 99999, 10),
-        });
-      }
-    }
-
-    setTimeout(() => resolve({ ...env, ['_renfe']: { timetable } }), 50);
+    fs.readFile('./mocks/renfe/timetable.json', (err, data) => {
+      if (err) throw err;
+      setTimeout(
+        () => resolve({ ...env, ['_renfe']: { timetable: JSON.parse(data) } }),
+        process.env.USE_RANDOM_DELAY ? createRandomDelay() : 50
+      );
+    });
   });
 };
 
