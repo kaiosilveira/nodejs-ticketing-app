baba9e4e810c783b105a1ea1d6ec8c76fbfbaf45
Kaio Silveira <silveira.kaio@outlook.com>
2022-08-08 18:33:44 +0100

    improve ticket filtering logic

--- src/ticketing-app/carriers/renfe/search/fetch-timetable/index.js
+++ src/ticketing-app/carriers/renfe/search/fetch-timetable/index.js
@@ -2,13 +2,15 @@ const fetchTimetable = async env => {
   return new Promise(resolve => {
     const timetable = [];
 
-    for (let i = 0; i < 23; i++) {
-      timetable.push({
-        date: new Date(2022, 8, 1, i, 30, 0),
-        origin: 1200,
-        destination: 1300,
-        price: 100000 + parseInt(Math.random() * 99999, 10),
-      });
+    for (let i = 0; i < 7; i++) {
+      for (let j = 0; j <= 23; j++) {
+        timetable.push({
+          date: new Date(2022, 8, i, j, 30, 0),
+          origin: 1200,
+          destination: 1300,
+          price: 100000 + parseInt(Math.random() * 99999, 10),
+        });
+      }
     }
 
     setTimeout(() => resolve({ ...env, ['_renfe']: { timetable } }), 50);
--- src/ticketing-app/carriers/renfe/search/filter-tickets-matching-date-time-delta/index.js
+++ src/ticketing-app/carriers/renfe/search/filter-tickets-matching-date-time-delta/index.js
@@ -1,19 +1,16 @@
+import moment from 'moment';
+
 const filterTicketsMatchingDateTimeDelta = env => {
   const timetable = env['_renfe'].timetable;
-  const dateTime = env.params.date;
+  const targetDate = env.params.date;
+  const previousDay = moment(targetDate).subtract(1, 'day');
+  const nextDay = moment(targetDate).add(1, 'day');
 
   return {
     ...env,
     ['_renfe']: {
       ...env['_renfe'],
-      result: timetable.filter(item => {
-        return (
-          item.date.getFullYear() === dateTime.getFullYear() &&
-          item.date.getMonth() === dateTime.getMonth() &&
-          item.date.getDate() === dateTime.getDate() &&
-          item.date.getHours() === dateTime.getHours()
-        );
-      }),
+      result: timetable.filter(item => moment(item.date).isBetween(previousDay, nextDay)),
     },
   };
 };
